20a36ca62c54e5267ce1635fb3628cc4f6d66b64
ArgeWorks <argeworks@icloud.com>
2018-03-20 20:48:40 +0200

    Added Portfolio and Start-Project sections

--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
 $( function() {
-    // Init Tabs
-    $( "#tabs" ).tabs();
+    // Init Tabs Services
+    $( "#tabs-services" ).tabs();
+    // Init Tabs Portfolio
+    $( "#tabs-portfolio" ).tabs();
 });
