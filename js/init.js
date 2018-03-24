43835c45d7e15cbba84036c17985769c98e25c0b
ArgeWorks <argeworks@icloud.com>
2018-03-24 15:04:29 +0200

    Added Reviews section and some fixes

--- js/init.js
+++ js/init.js
@@ -1,6 +1,13 @@
 $( function() {
     // Init Tabs Services
-    $( "#tabs-services" ).tabs();
+    $("#tabs-services").tabs();
     // Init Tabs Portfolio
-    $( "#tabs-portfolio" ).tabs();
+    $("#tabs-portfolio").tabs();
+
+    // Init Slick Slider
+    $(".reviews-slider").slick({
+        dots: true,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left" aria-hidden="true"></i></div></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right" aria-hidden="true"></i></div></div>',
+    });
 });
